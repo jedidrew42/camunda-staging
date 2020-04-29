2e75ff810a36de487a12466ce9119d7212d6f60b
Drew Norman <drew.norman@camunda.com>
2020-04-29 15:49:37 -0400

    changed some scripts

---
+++ themes/hugo-bootstrap-premium/static/js/matomo.js
@@ -0,0 +1,15 @@
+<script type="text/plain" data-type="application/javascript"
+        data-name="matomo">
+  var _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  _paq.push(['rememberConsentGiven']);
+  (function() {
+    var u="https://camunda.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/camunda.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
